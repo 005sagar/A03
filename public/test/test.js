4a199b4ed0b200e7d107a48765c477631ae3934c
Sagar <s527999@nwmissouri.edu>
2018-10-29 17:40:39 -0500

    commit

--- public/test/test.js
+++ public/test/test.js
@@ -9,12 +9,7 @@ QUnit.test('Testing calculateArea function with several sets of inputs', functio
     assert.equal(App.math(-67,-170), null, 'Tested with two negative numbers.');
     assert.equal(App.math(0,0), null, 'Tested with two zero numbers.');
 });
-QUnit.test('Testing calculateArea function with several sets of inputs', function (assert) {
-    assert.equal(App.math(67,160), 25.06, 'Tested with two positive numbers');
-    assert.equal(App.math(67,-110), null, 'Tested with one negative and one positive numbers. ');
-    assert.equal(App.math(-67,-170), null, 'Tested with two negative numbers.');
-    assert.equal(App.math(0,0), null, 'Tested with two zero numbers.');
-});
+
 
 
 
